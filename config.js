6c0587c5bbbdc0df701295895d05c02d303a9f2f
Kuulokenurkka <111870074+Kuulokenurkka@users.noreply.github.com>
2022-12-28 13:46:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ page_title = "Taajuusvasteiden vertailutyökalu";                     // Optiona
 const targets = [
   //{ type:"Neutral"   , files:["Diffuse Field", "Free Field", "Harman In-Room", "In-Ear Fidelity"] },
   //{ type:"Preference", files:["Antdroid","Harman 2013","Harman 2018", "Optimum Hifi","Sonarworks"] }
-  { type:"", files:["Harman 2018 Target"]}
+  { type:"", files:["Harman 2018 Target","Harman 2018 neutraalilla bassolla Target]}
 ];
 
 
